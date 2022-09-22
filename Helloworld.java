a47da5b013e24a1ea918bf89d582ea00bcd7bea3
RahulAid <raccess97@gmail.com>
2022-09-23 00:26:30 +0530

    Program to run a infinite Java Loop

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+public class Helloworld 
+		{
+	public static void main(String[] args)
+			{
+			while(true){
+			System.out.println("Hello world program in java") ;
+			try {
+			Thread.sleep(10000);
+			} catch (Exception ex) {}
+			}
+			}
+		}
