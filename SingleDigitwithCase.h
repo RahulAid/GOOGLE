7e93b2d3df96097c88626aa3131165a72b043d89
RahulAid <raccess97@gmail.com>
2022-09-26 08:44:29 +0530

    Print a Single digit using case

---
+++ SingleDigitwithCase.h
@@ -0,0 +1,36 @@
+RandomSingleDigit=$((RANDOM%9))
+	case $RandomSingleDigit in
+		0)
+		echo "0" 
+		;;
+		1)
+                echo "1"
+                ;;
+  		2)
+                echo "2"
+                ;;
+ 		3)
+                echo "3"
+                ;;
+ 		4)
+                echo "4"
+                ;;
+ 		5)
+                echo "5"
+                ;;
+ 		6)
+                echo "6"
+                ;;
+		6)
+                echo "6"
+                ;;
+		7)
+                echo "7"
+                ;;
+		8)
+                echo "8"
+                ;;
+		9)
+                echo "9"
+                ;;
+		esac
