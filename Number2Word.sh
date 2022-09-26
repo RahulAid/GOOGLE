314b7e46d81dc61ffc591543c3d26b2909b615e3
RahulAid <raccess97@gmail.com>
2022-09-26 09:56:02 +0530

    Print Number in Word

---
+++ Number2Word.sh
@@ -0,0 +1,32 @@
+read -p "Enter a single digit number : " a
+	  if [ $a -eq 0 ]
+	then
+	echo " The number entered is Zero " ;
+	  elif [ $a -eq 1 ] 
+   	then     
+	echo " The number entered is One "  
+	  elif [ $a -eq 2 ]
+        then 
+	echo " The number entered is Two " ;   
+	  elif [ $a -eq 3 ]
+        then 
+	echo " The number entered is Three " ; 
+	  elif [ $a -eq 4 ]
+        then
+	 echo " The number entered is Four " ;
+	  elif [ $a -eq 5 ]
+        then 
+	echo " The number entered is Five " ;
+	  elif [ $a -eq 6 ]
+        then 
+	echo " The number entered is Six " ;
+	  elif [ $a -eq 7 ]
+        then 
+	echo " The number entered is Seven " ;
+	  elif [ $a -eq 8 ]
+	then
+        echo " The number entered is Eight " ;
+	  elif [ $a -eq 9 ] 
+        then 
+	echo " The number entered is Nine " ;
+	  fi
