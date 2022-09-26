d85c225a7acb560506182b95b0b36f05c09a3ae1
RahulAid <raccess97@gmail.com>
2022-09-26 10:15:34 +0530

    Print Number in Word Using Case

---
+++ Number2WordWithCase.sh
@@ -0,0 +1,33 @@
+read -p "Enter a Single Digit Number : " a
+        case $a in
+                0)
+                echo " The number entered is ZERO"
+                ;;
+                1)
+                echo "The number entered is ONE"
+                ;;
+                2)
+                echo "The number entered is TWO"
+                ;;
+                3)
+                echo "The number entered is THREE"
+                ;;
+                4)
+                echo "The number entered is FOUR"
+                ;;
+                5)
+                echo "The number enetered is FIVE"
+                ;;
+                6)
+                echo "The number entered is SIX"
+                ;;
+                7)
+                echo "The number entered is SEVEN"
+                ;;
+                8)
+                echo "The number entered is EIGHT"
+                ;;
+                9)
+                echo "The number entered is NINE"
+                ;;
+                esac
